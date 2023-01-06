e8e04046d14677ac1c459b4d23608699fa0b5369
Shariful Islam <shariful.islam@questionpro.com>
2023-01-06 17:46:25 +0600

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+const env = require('dotenv').config();
+
+if (env.error) {
+  throw env.error;
+}
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      url: process.env.JAWSDB_URL,
+      entities: ['dist/**/*.entity.js'],
+      synchronize: Boolean(process.env.DB_SYNCHRONIZE),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
